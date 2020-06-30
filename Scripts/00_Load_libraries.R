c9f0f952d21ba1afdc5a2399466657544344c40e
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-06-30 13:10:49 +0300

    Add script 00 load lib + first script settings

---
+++ Scripts/00_Load_libraries.R
@@ -0,0 +1,124 @@
+##########################################################################################
+#
+#                                        LOAD LIBRARIES
+#
+##########################################################################################
+# April 2020
+# Antoine Becker-Scarpitta
+# R, Rstudio need to be updated
+# run INSTALATION to install all packages before loading them
+
+  
+
+# # INSTALATION
+# 
+# # General
+# install.packages("devtools")
+# install.packages("knitr")
+# 
+# 
+# # Community
+# install.packages("vegan")
+# install.packages("ade4")
+# install.packages("FD")
+# install.packages("RVAideMemoire")
+# install.packages("indicspecies")
+# install.packages("mclust")
+# install.packages("labdsv")
+# install.packages("betapart")
+# 
+# 
+# # Data managment
+# install.packages("reshape")
+# install.packages("reshape2")
+# install.packages("plyr")
+# install.packages("dplyr")
+# install.packages("tidyr")
+# install.packages("gridExtra")
+# install.packages("stringr")
+# install.packages("stringi")
+# install.packages("data.table")
+# install.packages("Taxonstand")
+# 
+# 
+# # Analysis
+# install.packages("lme4")
+# install.packages("effects")
+# install.packages("AICcmodavg")
+# install.packages("MuMIn")
+# install.packages("car")
+# install.packages("arm")
+# install.packages("effects")
+# install.packages("bbmle")
+# install.packages("sjPlot")
+# install.packages("multcomp")
+# install.packages("lmerTest")
+# install.packages("simr")
+# 
+# 
+# # Plot
+# install.packages("ggplot2")
+# install.packages("ggrepel")
+# install.packages("ggpubr")
+# install.packages("rworldmap")
+# install.packages("mapdata")
+
+
+
+#  LOAD
+
+# General
+library(devtools)
+library(knitr)
+
+
+# Community
+library(vegan)
+library(ade4)
+library(FD)
+library(RVAideMemoire)
+library(indicspecies)
+library(mclust)
+library(labdsv)
+library(betapart)
+
+
+# Data managment
+library(reshape)
+library(reshape2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(gridExtra)
+library(stringr)
+library(stringi)
+library(data.table)
+library(Taxonstand)
+
+
+# Analysis
+library(lme4)
+library(effects)
+library(AICcmodavg)
+library(MuMIn)
+library(car)
+library(arm)
+library(effects)
+library(stats)
+library(bbmle)
+library(sjPlot)
+library(multcomp)
+library(lmerTest)
+library(simr)
+
+
+# Plot
+library(ggplot2)
+library(ggrepel)
+library(ggpubr)
+library(rworldmap)
+library(mapdata)
+
+
+
+
--- Scripts/01_DB_creation.R
+++ Scripts/01_DB_creation.R
@@ -1,1 +1,22 @@
-#  Zack Flowering project
+#######################################################################################
+#
+#                           Greenland Flowering project
+#                                 01 - DB CREATION
+#
+#######################################################################################
+# Antoine Becker-Scarpitta
+# June 2020
+
+#  clean R work-space
+rm(list=ls())
+
+# load library
+# R, Rstudio need to be updated
+# Library need to be installed (see 00_Load_libraries script)
+source("Scripts/00_Load_libraries.r")
+
+
+
+
+
+
