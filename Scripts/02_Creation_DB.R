cb08befb99f74f56d63326113c3ed570c614cf89
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-06-30 18:21:46 +0300

    02_CReation_DB: Rbind all datasets Select "TOTALCOUNT" lines only Split Date in Year, Month, Days,

--- Scripts/02_Creation_DB.R
+++ Scripts/02_Creation_DB.R
@@ -16,4 +16,31 @@ rm(list=ls())
 source("Scripts/00_Load_libraries.r")
 
 # Load 01 - Import_DB
-source("Scripts/01_Load_libraries.r")
+source("Scripts/01_Import_DB.r")
+
+
+
+# RBIND ALL DB + 
+# SELECT "TOTALCOUNT"
+
+
+
+# Rbind all datasets together
+flow0 <- rbind(Nsal, Nsil, Zcas, Zdry, Zpap, Zsal, Zsax, Zsil)
+
+# subset only line with "TOTALCOUNT"
+flow1 <- subset(flow0, TotalCount=="TOTALCOUNT")
+
+# split Date col, to extract year, month, day in new cols
+flow <- cbind(str_split_fixed(flow[,"Date"], '-', n=3), flow)
+colnames(flow) <- c("Year", "Month", "Day", "Site", "Date", "Plot", "Section",
+                    "TotalCount", "Flower_var", "Value")
+
+
+
+
+
+
+
+
+
