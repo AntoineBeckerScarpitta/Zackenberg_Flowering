86312304e64ee01e2daa7d2fc30b1fe5897fca55
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-06-30 15:03:32 +0300

    read.all CSV select column add site name

--- Scripts/01_DB_creation.R
+++ Scripts/01_DB_creation.R
@@ -18,5 +18,102 @@ source("Scripts/00_Load_libraries.r")
 
 
 
+### READ DATASET ###
+
+### Zackenberg (6 species)
+# Cassiope
+Zcas0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,
+                 na.strings = c("","NA"))
+# delete remarks and comments columns
+Zcas <- Zcas0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zcas$Site <- 'Zackenberg'
+
+# Dryas
+Zdry0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Zdry <- Zdry0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zdry$Site <- 'Zackenberg'
+
+
+# Papaver
+Zpap0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Zpap <- Zpap0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zpap$Site <- 'Zackenberg'
+
+
+# Saxifraga
+Zsax0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Zsax <- Zsax0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zsax$Site <- 'Zackenberg'
+
+
+
+# Silene
+Zsil0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Zsil <- Zsil0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zsil$Site <- 'Zackenberg'
+
+
+
+# Salix
+Zsal0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Zsal <- Zsal0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with ZACK
+Zsal$Site <- 'Zackenberg'
+
+
+
+
+### Nuuk
+# 2 species
+# Salix
+Nsal0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                 stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Nsal <- Nsal0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with NUUK
+Nsal$Site <- 'Nuuk'
+
+
+
+# Silene
+Nsil0 <- read.csv("data/datasets/View_BioBasis_Zackenberg_Data_Vegetation_Cassiope_phenology_and_total_count.csv", 
+                  stringsAsFactors=FALSE, header=TRUE,  sep="\t", strip.white = T,na.strings = c("","NA"))
+# delete remarks and comments
+Nsil <- Nsil0[ ,c("Date", "Plot", "Section", "Buds", "Flowers", 
+                  "Senescent", "TotalFlowering", "TotalCount")]
+#  add SITE column with NUUK
+Nsil$Site <- 'Nuuk'
+
+
+
+
+
+
+
+
+
 
 
