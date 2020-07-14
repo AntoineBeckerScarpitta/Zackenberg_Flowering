a507a01b8d03a30f008cfdb7d471cf6dc413631b
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-07-14 11:51:54 +0300

    Delete K and W plots

--- Scripts/02_Creation_DB.R
+++ Scripts/02_Creation_DB.R
@@ -33,6 +33,11 @@ flow0 <- rbind(Nsal, Nsil, Zcas, Zdry, Zpap, Zsal, Zsax, Zsil)
 # subset only line with "TOTALCOUNT"
 flow <- subset(flow0, TotalCount=="TOTALCOUNT")
 
+# delete plot K and W
+flow <-flow[!flow$Plot %in% c("K1C","K2C","K3C",'K4C',"K5C","W1C","W2C","W3C",
+                  'W4C',"W5C","K3S","K4S","K5S","W3S","W4S","W5S", 
+                  "K1S","K2S","W1S","W2S"), ]
+
 # split Date col, to extract year, month, day in new cols
 flow <- cbind(str_split_fixed(flow[,"Date"], '-', n=3), flow)
 colnames(flow) <- c("Year", "Month", "Day", "Site", "Date", "Plot", "Section",
@@ -43,5 +48,7 @@ flow[flow$Value==-9999, 'Value'] <- NA
 
 # data as numeric
 flow[c("Year", "Month", "Day")] <- sapply(flow[c("Year", "Month", "Day")], as.numeric)
+# flow[c("Year", "Site", "Plot", "Section", "Species")] <- sapply(flow[c("Year","Site","Plot","Section","Species")], as.factor)
+
 ### END
 
