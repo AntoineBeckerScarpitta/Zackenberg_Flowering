1a130662bae174e7ba71ef70b491b49d8cc6713f
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-09-16 19:07:54 +0300

    NEw graph on flowering variable distributions

--- Scripts/03_Exploration _graphs.R
+++ Scripts/03_Exploration _graphs.R
@@ -15,7 +15,7 @@ source("Scripts/02_Creation_DB.r")
 
 
 
-#  PLOT LEVEL 
+#  TEMPORAL TRENDS AT PLOT LEVEL 
 # 1 - Flower density per m2  ## --------------------------------------------
 flow_den <- ggplot(flow_tot_plot, aes(Year, Flow_m2, group=Plot, color=Plot)) +
   geom_point() +
@@ -39,13 +39,12 @@ flow_tot <- ggplot(flow_tot_plot, aes(Year, TotalFlower, group=Plot, color=Plot)
 #  graph
 gridExtra::grid.arrange(flow_den, flow_tot, ncol=2, 
                         top = "Plot level temporal trends in flowering at Zackenberg")
-
 ### END PLOT LEVEL ## ------------------------------------------------------
 
 
 
 
-#  SPECIE LEVEL 
+#  TEMPORAL TRENDS AT SPECIE LEVEL 
 # 3 - Flower density per m2  ## --------------------------------------------
 flow_den2 <- ggplot(flow_tot_plot, aes(Year, Flow_m2, group=Species, color=Species)) +
   geom_point() +
@@ -69,8 +68,30 @@ flow_tot2 <- ggplot(flow_tot_plot, aes(Year, TotalFlower, group=Species, color=S
 #  graph
 gridExtra::grid.arrange(flow_den2, flow_tot2, ncol=2, 
                         top = "Temporal flowering trends at species level in Zackenberg")
-
 ### END SP LEVEL ## ------------------------------------------------------------------
 
 
+
+# DISTRIBUTION OF FLOWERING VARIABLES 
+# DENSITY
+distdens <- ggplot(flow_tot_plot, aes(x=Flow_m2, fill=Species)) + 
+  geom_density(alpha=.3) +
+  labs(y='Density', x='Flowering density/m2 ') +
+  ggtitle('Distribution of Flowering density') +
+  facet_grid(Species~ ., scales="free") +
+  theme_linedraw() +
+  theme(legend.position = "none")
+
+# TOTAL
+disttot <- ggplot(flow_tot_plot, aes(x=TotalFlower, fill=Species)) + 
+  geom_density(alpha=.3) +
+  labs(y='Density', x='Total Flowers') +
+  ggtitle('Distribution of Total Flowering') +
+  facet_grid(Species~ ., scales="free") +
+  theme_linedraw() +
+  theme(legend.position = "none")
+
+#  graph
+gridExtra::grid.arrange(distdens, disttot, ncol=2, 
+                        top = "Distribution of flowering variable per species")
 
