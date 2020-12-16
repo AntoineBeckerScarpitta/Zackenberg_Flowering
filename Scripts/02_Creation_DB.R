88395dba1dae0781560eccd573f11dc5ae472142
AntoineBeckerScarpitta <antoine.becker.scarpitta@protonmail.com>
2020-12-16 15:21:40 +0200

    Selection for Salix at Zackenberg done

--- Scripts/02_Creation_DB.R
+++ Scripts/02_Creation_DB.R
@@ -73,26 +73,51 @@ Nuuk_tot_plot$Flow_m2 <- round(Nuuk_tot_plot$TotalFlower/Nuuk_tot_plot$Plot_size
 
 
 ####  ZACKENBERG ####
-# Salix : sum male + sum female (with 50/50 buds)
+# Salix :Need to rearrange : sum male + sum female (with 50/50 buds)
+# divided Date into Year, months, days and select only Year
 Zsal <- cbind(str_split_fixed(Zsal[,"Date"], '-', n=3), Zsal)
 colnames(Zsal) <- c("Year", "Month", "Day", "Site", "Date", "Plot", "Section",
                     "TotalCount", "Flower_var", "Value", "Species")
 
-Zsal_sex <- as.data.frame(Zsal %>% filter(TotalCount=="TOTALCOUNT", 
-                               Value != -9999) %>%
-                        group_by(Site, Date, Plot, TotalCount, Flower_var, Species) %>%
+# sleect TOTALCOUNT, remove -9999, sum at plot level and select only Total Male, Female and Buds
+Zsal_sel <- as.data.frame(Zsal %>% filter(TotalCount=="TOTALCOUNT", Value != -9999) %>%
+                        group_by(Site, Year, Plot, Flower_var, Species) %>%
                         summarise(Value=sum(Value)) %>% 
                         ungroup() %>%
                         filter(Flower_var %in% c("Total_Female", "Total_Male", "Buds"),
-                               Value>0) %>%
-                        mutate(Species=ifelse(Flower_var=="Total_Male", "SAL_m", 
-                                              ifelse(Flower_var=="Total_Female", "SAL_f", "SAL_b"))))
-                        
-                        
-                      
+                               Value>0)) 
+# select only Buds
+buds <- Zsal_sel%>%filter(Flower_var=="Buds")
+# select only total male and female
+smf <- Zsal_sel%>%filter(Flower_var!="Buds")
+
+# Final arrangement
+Zsal_tot_plot <- as.data.frame(smf %>% pivot_wider(id_cols=c(Site, Year, Plot, Species),
+                    names_from=Flower_var, values_from=Value, values_fill = 0) %>%
+  # merge Buds and smf (need one row for each obs) 
+  left_join(.,  dplyr::select(buds, Year, Plot, Value), by=c("Year", "Plot")) %>%
+  replace(is.na(.), 0) %>%
+  # 50% buds for male 50% for female
+    mutate(Total_Male_b=Total_Male + Value*0.5, 
+         Total_Female_b=Total_Female + Value*0.5) %>%
+  dplyr::select(c(Site, Year, Species, Plot, Total_Male_b, Total_Female_b)) %>%
+  pivot_longer(cols=c(Total_Male_b, Total_Female_b), names_to="Flower_var", values_to="TotalFlower") %>%
+  filter(TotalFlower>0) %>% ungroup() %>%
+  #  created a new species level Salix_male and Salix_female
+  mutate(Species=ifelse(Flower_var=="Total_Male_b", "SAL_male","SAL_female")) %>%
+  # remove Flowe_var (which was total male, female)
+  dplyr::select(., -Flower_var) %>%
+  # merge Plot size
+  left_join(., Plot_size[,c('Plot_size', 'Plot')], by="Plot") %>%
+  # rearrange col order to rbind other species
+  dplyr::select(Site, Year, Species, Plot, Plot_size, TotalFlower) %>%
+  # calculate the flowering density
+  mutate(Flow_m2=round(TotalFlower/Plot_size, 2)))
+#END SALIX---- 
+         
 
 
-
+# Other SPECIES at ZACK
 # Rbind all ZACK datasets together except SALIX
 Zack0 <- rbind(Zcas, Zdry, Zpap, Zsax, Zsil)
 
@@ -134,7 +159,7 @@ Zack_tot_plot$Flow_m2 <- round(Zack_tot_plot$TotalFlower/Zack_tot_plot$Plot_size
 
 
 # Combined ZACKENBERG AND NUUK in 1 table
-flow <- as.data.frame(rbind(Zack_tot_plot, Nuuk_tot_plot))
+flow <- as.data.frame(rbind(Zack_tot_plot, Nuuk_tot_plot, Zsal_tot_plot))
 
 # data as numeric or factor
 flow[,"Year"] <- as.numeric(flow[,"Year"])
@@ -155,7 +180,8 @@ flow$trans_Flow_m2 <- flow$Flow_m2 + 0.001
 # remove temprary files
 remove("Nuuk_all", "Nuuk_all_sub", "Nuuk_all0",  "Zack", "Zack_sub", "Zack0", "Zack1", 
       "Zcas", "Zcas0", "Zcas1", "Zdry", "Zdry0", "Zdry1", "Zpap", "Zpap0", "Zpap1", 
-      "Zsal", "Zsal0", "Zsal1", "Zsax", "Zsax0", "Zsax1", "Zsil", "Zsil0", "Zsil1")
+      "Zsal", "Zsal0", "Zsal_sel", "buds", "smf", "Zsal1", "Zsax", "Zsax0", "Zsax1", 
+      "Zsil", "Zsil0", "Zsil1")
 
 
 
