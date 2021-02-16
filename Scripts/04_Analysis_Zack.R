df7c9bf43ff799d85d72accc36b04ab1ffb0f546
Antoine Becker-Scarpitta <antoine.becker.scarpitta@protonmail.com>
2021-02-16 15:07:49 +0200

    renames model to align with script 05 readRDS for laura + model exploration plot

--- Scripts/04_Analysis_Zack.R
+++ Scripts/04_Analysis_Zack.R
@@ -82,32 +82,32 @@ flow_snow_clim_z <- flow_snow_clim_z %>%
 ## 2 - MODELS -----------------------------------------------------------------------
 
 #  MODEL 1: flow(t) ~ Sp * Year + flow(t-1) + ranef(plot)
-mod1_z <- lmer(log(trans_Flow_m2) ~ Species * as.numeric(Year) + lag_trans_Flow_m2 +
+mod_basic_z <- lmer(log(trans_Flow_m2) ~ Species * Year + lag_trans_Flow_m2 +
              (1|Plot),
              data= flow_snow_clim_z,
              REML=T, na.action=na.omit)
-summary(mod1_z)
-saveRDS(mod1_z, "results/models/mod_basic_z.rds")
+summary(mod_basic_z)
+saveRDS(mod_basic_z, "results/models/mod_basic_z.rds")
 
 
 #  MODEL 2: 
 # flow(t)~ Sp*clim(summer) + Sp*clim(fall-1) + Sp*SnowMelt + flow(t-1) + ranef(plot)
-mod2_z <- lmer(log(trans_Flow_m2) ~ Species * Temp_summer + Species * lag_Temp_fall +
+mod_full_z <- lmer(log(trans_Flow_m2) ~ Species * Temp_summer + Species * lag_Temp_fall +
                                   Species * snowmelt_DOY  + lag_trans_Flow_m2 + (1|Plot),
              data=flow_snow_clim_z, 
              REML=T, na.action=na.omit)
-summary(mod2_z)
-saveRDS(mod2_z, "results/models/mod_full_z.rds")
+summary(mod_full_z)
+saveRDS(mod_full_z, "results/models/mod_full_z.rds")
 
 
 #  MODEL 3: ranef plot structured by year
 # flow(t)~ Sp*clim(summer) + Sp*clim(fall-1) + Sp*SnowMelt + ranef(plot/year)
-mod3_z <- lmer(log(trans_Flow_m2) ~ Species * Temp_summer + Species * lag_Temp_fall +
+mod_full_2_z <- lmer(log(trans_Flow_m2) ~ Species * Temp_summer + Species * lag_Temp_fall +
                                   Species * snowmelt_DOY  + (1|Plot/Year),
              data=flow_snow_clim_z, 
              REML=T, na.action=na.omit)
-summary(mod3_z)
-saveRDS(mod3_z, "results/models/mod_full_ranef_plot_year_z.rds")
+summary(mod_full_2_z)
+saveRDS(mod_full_2_z, "results/models/mod_full_ranef_plot_year_z.rds")
 #-----------------------------------------------------------------------------------=
 
 # models used different data, can't compare them
@@ -115,42 +115,79 @@ saveRDS(mod3_z, "results/models/mod_full_ranef_plot_year_z.rds")
 
 
 # R2c, m
-MuMIn::r.squaredGLMM(mod1_z)
-MuMIn::r.squaredGLMM(mod2_z)
-MuMIn::r.squaredGLMM(mod3_z)
+MuMIn::r.squaredGLMM(mod_basic_z)
+MuMIn::r.squaredGLMM(mod_full_z)
+MuMIn::r.squaredGLMM(mod_full_2_z)
 
 
-# POSTHOC TEST ON mod2_z (full model)
-# # posthoc test
-# emmeans(mod2_z, list(pairwise ~ Species), adjust = "tukey")
-# 
-# 
-# # # r2 marginal et conditionnels des effets fixes
-# r2glmm::r2beta(mod2_z, method = 'nsj') 
-# # check les methodes, ?a peux faire une diff?rence...
-# 
-# # # plot les "graph criticism plots"
-# LMERConvenienceFunctions::mcp.fnc(mod2_z)$rstand
-# 
-# 
-# # # plot rapide des effects fixes significatif sous forme de graph
-# plot(effects::allEffects(mod2_z),multiline=T,rug=F,ci.style = "line",show.data=T)
-# 
-# 
-# # # plot rapide de tout les effects fixes ( la ligne verticale du 0 ?tant le "niveau 1" de chaque effet fixe)
-# sjPlot::plot_model(mod2_z,show.values = T,vline.color = "grey",value.offset = -0.3)
-# 
-# 
-# # plot des effets fixes en d?tail
-# # ,show.data=T si tu veux voir les points
-# # ,type = "eff" si tu veux voir les effets "reels" ; ,type ="pred" si tu veux voir les effets pr?dits par le mod?le
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("Species"),show.data=F)+theme_bw()
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("Species","lag_trans_Flow_m2"),show.data=F)+theme_bw()
-# 
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("Species","snowmelt_DOY"),show.data=F)+theme_bw()
-# 
-# 
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("snowmelt_DOY", "Species"),show.data=F)+theme_bw()
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("lag_trans_Flow_m2", "Species"),show.data=F)+theme_bw()
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("Temp_summer", "Species"),show.data=F)+theme_bw()
-# sjPlot::plot_model(mod2_z, type = "eff", terms = c("lag_Temp_fall", "Species"),show.data=F)+theme_bw()
+# POSTHOC TEST ON mod_full_z (full model)
+# posthoc test
+emmeans(mod_full_z, list(pairwise ~ Species), adjust = "tukey")
+
+
+# # r2 marginal et conditionnels des effets fixes
+r2glmm::r2beta(mod_full_z, method = 'nsj')
+# check les methodes, ?a peux faire une diff?rence...
+
+# # plot les "graph criticism plots"
+LMERConvenienceFunctions::mcp.fnc(mod_full_z)$rstand
+
+
+# # plot rapide des effects fixes significatif sous forme de graph
+plot(effects::allEffects(mod_full_z),multiline=T,rug=F,ci.style = "line",show.data=T)
+
+
+# # plot rapide de tout les effects fixes ( la ligne verticale du 0 ?tant le "niveau 1" de chaque effet fixe)
+sjPlot::plot_model(mod_full_z,show.values = T,vline.color = "grey",value.offset = -0.3)
+
+
+# plot des effets fixes en d?tail
+# ,show.data=T si tu veux voir les points
+# ,type = "eff" si tu veux voir les effets "reels" ; ,type ="pred" si tu veux voir les effets pr?dits par le mod?le
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("Species"),show.data=F)+theme_bw()
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("Species","lag_trans_Flow_m2"),show.data=F)+theme_bw()
+
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("Species","snowmelt_DOY"),show.data=F)+theme_bw()
+
+
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("snowmelt_DOY", "Species"),show.data=F)+theme_bw()
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("lag_trans_Flow_m2", "Species"),show.data=F)+theme_bw()
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("Temp_summer", "Species"),show.data=F)+theme_bw()
+sjPlot::plot_model(mod_full_z, type = "eff", terms = c("lag_Temp_fall", "Species"),show.data=F)+theme_bw()
+
+
+
+
+# print table model ZACK
+#computation of p-values is based on conditional F-tests with Kenward-Roger
+tab_model(mod_full_z, 
+          p.val = "kr", 
+          show.df = TRUE)
+
+#model table to compare model
+tab_model(mod_full_z, mod_full_2_z,
+          p.val = "kr", 
+          show.df = TRUE, 
+          dv.labels = c("Full(1|plot)", "Full_2(1|Plot/Year)"))
+
+#Forest plot on ZACK
+plot_model(mod_full_z , 
+           show.intercept = TRUE, 
+           show.values=T, 
+           value.offset = -0.3, 
+           show.p = TRUE, 
+           vline.color = "red", 
+           title = "Zackenberg") + theme_bw()
+
+
+
+
+
+
+
+
+
+
+
+
+
